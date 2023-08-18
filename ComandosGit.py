4947adce8c8d607b6fe0d5ff1c67a29ac62f7070
Patrick Andres Veroiza Palma <pveroiz.15@losheroes.cl>
2023-08-17 21:43:08 -0400

    Nuevo Archivo py

---
+++ ComandosGit.py
@@ -0,0 +1,12 @@
+print('git clone')
+print('1. git add')
+print('2. git commit -m "mensjae" ')
+print('3. git push')
+
+### Merge ###
+
+print('Otros comandos')
+print('git pull')
+print('git branch')
+print('git merge')
+print('git checkout -b nombreRama')
