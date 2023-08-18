a3dc5527919383180b4eb064f96fee5c91c9aaf7
Patrick Andres Veroiza Palma <pveroiz.15@losheroes.cl>
2023-08-17 21:19:18 -0400

    Codigo Nuevo

---
+++ holamundo.py
@@ -0,0 +1,11 @@
+print("Hola Mundo")
+a = 3
+b = 5
+c= a+b
+
+print (c)
+
+nombre = "patrick"
+edad = 20
+
+print(f'mi nombre es:',nombre,'y tengo ',edad ,' años')
---
+++ tiposdedatos.py
@@ -0,0 +1,20 @@
+#Texto
+"string"
+'string'
+
+"""string
+        string"""
+        
+4 #int
+4.4 #float
+True #booleano
+
+##Variables
+#se declara y luego se define
+
+nombre = 'patrick'
+Bienvenida = f"Hola {nombre}"
+print(Bienvenida)
+
+
+
